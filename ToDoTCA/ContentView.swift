45a1c7b053070b46baf90b08ebf7101d765af7c6
Doris <doris070714@gmail.com>
2025-02-08 09:18:17 +0100

    Initial Commit

---
+++ ToDoTCA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoTCA
+//
+//  Created by App_team on 2025/2/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoTCA/ToDoTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoTCAApp.swift
+//  ToDoTCA
+//
+//  Created by App_team on 2025/2/8.
+//
+
+import SwiftUI
+
+@main
+struct ToDoTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
